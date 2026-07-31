5d491418cc8ee93a8027a8cb39ef2f1b9ea036a8
root <root@DESKTOP-8DGNCKN.localdomain>
2026-07-31 01:07:46 -0700

    Created the WalletConnect component and implemented MetaMask wallet connection.

--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -1,37 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { connectWallet } from "@/lib/web3";
 
 export default function WalletConnect() {
   const [address, setAddress] = useState("");
+  const [loading, setLoading] = useState(false);
 
+// Connect Wallet
   const handleConnect = async () => {
-    const { address } = await connectWallet();
-    setAddress(address);
+    try {
+      setLoading(true);
+      const { address } = await connectWallet();
+      setAddress(address);
+    } catch (error){
+      console.error(error);
+      alert("Failed to connect wallet.");
+    } finally {
+      setLoading(false);
+    }
   };
-
-  const handleDisconnect = () => {
-    setAddress("");
-  };
-
-  return (
-    <>
-      {address ? (
-        <button
-          onClick={handleDisconnect}
-          className="bg-red-600 text-white px-4 py-2 rounded"
-        >
-          Disconnect ({address.slice(0, 6)}...)
-        </button>
-      ) : (
-        <button
-          onClick={handleConnect}
-          className="bg-black text-white px-4 py-2 rounded"
-        >
-          Connect Wallet
-        </button>
-      )}
-    </>
-  );
 }
