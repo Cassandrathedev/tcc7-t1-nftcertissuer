8779f7d12f4cffdbe8db0f754d019bc75575620c
root <root@DESKTOP-8DGNCKN.localdomain>
2026-08-02 13:14:49 -0700

    Created the issue certificate page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
           </Link>
 
           <Link
-            href="/issue"
+            href="@/components/CertificateForm"
             className="text-gray-700 transition hover:text-blue-600"
           >
             Issue
