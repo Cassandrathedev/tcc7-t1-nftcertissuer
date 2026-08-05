e76948645443f9b960f95f3e76e3338fc3ecf40c
Cassandra Onu <cassandrathedev@gmail.com>
2026-08-05 18:05:59 +0100

    Update CertificateForm.tsx

--- components/CertificateForm.tsx
+++ components/CertificateForm.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Contract } from "ethers";
 import { getSigner } from "@/lib/web3";
-import NFTCertissuer from "@/contracts/out/NFTCertIssuer.sol/NFTCertissuer.json";
+import NFTCertissuer from "@/contracts/out/NFTCertIssuer.sol/NFTCertIssuer.json";
 
 const CONTRACT_ADDRESS =
 process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!;
